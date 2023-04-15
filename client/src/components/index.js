64d558e48f6cfc2d785a5ac9d149bc4e39e62495
Michael Stafford <michaelstafford808@gmail.com>
2023-04-14 18:43:20 -1000

    imported react icons, added css gradient card, and grid to Welcome component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Loader } from './Loader';
-export { default as Welcome } from './Loader';
+export { default as Welcome } from './Welcome';
 export { default as NavBar } from './NavBar';
 export { default as Footer } from './Footer';
 export { default as Transactions } from './Transactions';
