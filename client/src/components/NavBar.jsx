9c11e9119af6f4cb0ce48279aeff06d603ddfd72
Michael Stafford <michaelstafford808@gmail.com>
2023-04-14 17:10:26 -1000

    added toggleMenu in NavBar component along with css styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
       <div className='md:flex-[0.5] flex-initial justify-center items-center'>
         <img src={logo} alt="logo" className='w-32 cursor-pointer'/>
       </div>
-      <ul className='text-white md:flex flex-row items-center flex-initial'>
+      <ul className='text-white md:flex hidden flex-row items-center flex-initial'>
           {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
             <NavbarItem key={item + index} title={item}/>
           ))}
@@ -34,6 +34,19 @@ const NavBar = () => {
           {toggleMenu ? 
           <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} /> 
           : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)} /> }
+          {toggleMenu && (
+            <ul
+            className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
+            >
+              <li className='text-xl w-full my-2'>
+                  <AiOutlineClose onClick={() => setToggleMenu(false)}/>
+              </li>
+              {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                  <NavbarItem key={item + index} title={item} classProps='my-2 text-lg'/>
+              ))}
+            </ul>
+          )}
       </div>
     </nav>
   )
