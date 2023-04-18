bfc70ebda6377b234b9f28aa4fcd012a02b6e838
Michael Stafford <michaelstafford808@gmail.com>
2023-04-17 14:04:46 -1000

    created functionality which shows if metamask wallet is connected from the start. Also added logic for connecting the account to application

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,14 +1,14 @@
+import React, { useContext } from 'react'
 import { AiFillPlayCircle } from "react-icons/ai"
 import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle } from 'react-icons/bs'
 import { Loader } from './'
 
+import { TransactionContext } from "../context/TransactionContext"
+
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 const Welcome = () => {
-
-  const connectWallet = () => {
-
-  }
+  const { connectWallet, currentAccount } = useContext(TransactionContext);
 
   const handleSubmit = () => {
 
@@ -35,6 +35,7 @@ const Welcome = () => {
             <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
               Explore the crypto world Buy and sell cryptocurencies easily on Krypt Block 
             </p>
+            {!currentAccount && (
             <button
               type="button"
               onClick={connectWallet}
@@ -42,6 +43,8 @@ const Welcome = () => {
             >
               <p className="text-white text-base font-semibold">Connect Wallet</p>
             </button>
+            )}
+            
             <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
               <div className={`rounded-tl-2xl ${commonStyles}`}>
                 Reliability 
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x9304A1998bAF593F8E6dCcA1fCAB5b57E3946A0c';
